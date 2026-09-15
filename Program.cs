77eca34081e46ed81c45f59a0e62de55ae128987
gavril0va <gavrilova.veronika38d@gmail.com>
2026-09-15 11:17:55 +0300

    Lab04: Camoctoyatelnye zadachi

--- Program.cs
+++ Program.cs
@@ -1,37 +1,56 @@
-﻿//int age = 15;
-//if (age >= 18) {
-    //Console.WriteLine("Доступ разрешён");
-//}
-//Console.WriteLine( "Программа продолжает работу");
+﻿// //int age = 15;
+// //if (age >= 18) {
+//     //Console.WriteLine("Доступ разрешён");
+// //}
+// //Console.WriteLine( "Программа продолжает работу");
 
 
-// int age = 15;
-// if (age >= 18) {
-//     Console.WriteLine("Доступ разрешён");
-// } else {
-// Console.WriteLine("Доступ запрещён");
-// Console.WriteLine($"До совершеннолетия осталось ждать: {18 - age} лет");
+// // int age = 15;
+// // if (age >= 18) {
+// //     Console.WriteLine("Доступ разрешён");
+// // } else {
+// // Console.WriteLine("Доступ запрещён");
+// // Console.WriteLine($"До совершеннолетия осталось ждать: {18 - age} лет");
+// // }
+
+
+// // int age = 100;
+// // if (age < 13) {
+// //     Console.WriteLine("Ребёнок");
+// // } else if (age < 18) {
+// //     Console.WriteLine("Подросток");
+// // } else if (age < 60) {
+// //     Console.WriteLine("Взрослый");
+// // } else {
+// //     Console.WriteLine("Пенсионер");
+// // }
+
+
+// int age = 16;
+// double height = 1.55;
+// bool hasAdult = true; 
+
+// if (age >= 14 && (height >= 1.5 || hasAdult)) {
+//     Console.WriteLine("Можно кататься");
+// } else
+// {
+//     Console.WriteLine("Пока нельзя");
 // }
 
 
-// int age = 100;
-// if (age < 13) {
-//     Console.WriteLine("Ребёнок");
-// } else if (age < 18) {
-//     Console.WriteLine("Подросток");
-// } else if (age < 60) {
-//     Console.WriteLine("Взрослый");
-// } else {
-//     Console.WriteLine("Пенсионер");
-// }
+//ЗАДАЧА А
+Console.WriteLine();
+Console.WriteLine("Чётное или нечётное");
 
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
 
-int age = 16;
-double height = 1.55;
-bool hasAdult = true;
-
-if (age >= 14 && (height >= 1.5 || hasAdult)) {
-    Console.WriteLine("Можно кататься");
+if (number % 2 == 0) {
+    Console.WriteLine("Число чётное");
 } else {
-    Console.WriteLine("Пока нельзя");
+    
+    Console.WriteLine("Число нечётное");
 }
+
+
+// ЗАДАЧА Б
